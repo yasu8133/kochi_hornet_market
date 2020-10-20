0a0661d9f89f174eeef9faaceae2fa4bbf86639d
TakayukiYorita <yori922@icloud.com>
2020-10-21 00:11:05 +0900

    server-side10_20

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :last_name, :kana_family_name, :kana_last_name, :birth])
   end
 end
