edad4654afe9b6cc60176205e748ae2b45b448fb
kazushi07 <reddwarfthespaceship@gmail.com>
2020-10-27 20:42:50 +0900

    fix_routes

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,8 +3,6 @@ class Image < ApplicationRecord
   belongs_to :exhibition
   # 画像がない時はエラー
   validates :image, presence: true
-end
-
   belongs_to :exhibition, optional: true
   mount_uploader :image_url, ImageUploader
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  # devise_for :users
+  devise_for :users
   root 'items#index'
   
-  # 出品機能実装のための仮置きです 堅田
+  # 出品機能実装のための仮置きです 堅田もしexhibitionコントローラを修正したなら削除して10行目のresourcesをexhibitionsにすること
   resources :exhibitions, except: :show
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
