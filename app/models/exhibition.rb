479888d26f0980c14072cd1d0cf938602f20ceb1
kazushi07 <reddwarfthespaceship@gmail.com>
2020-10-27 20:41:02 +0900

    fix_varidation

--- app/models/exhibition.rb
+++ app/models/exhibition.rb
@@ -58,7 +58,7 @@ class Exhibition < ApplicationRecord
 
 end
 
-class Item < ApplicationRecord
-  has_many :images, dependent: :destroy
-  accepts_nested_attributes_for :images, allow_destroy: true
-end
+# class Item < ApplicationRecord
+#   has_many :images, dependent: :destroy
+#   accepts_nested_attributes_for :images, allow_destroy: true
+# end
