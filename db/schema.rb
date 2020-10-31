47e268dfd8c38d626d606c773eac6697ac13cb51
moichimasaki <48578452+moichimasaki@users.noreply.github.com>
2020-10-31 20:57:20 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_28_114104) do
+ActiveRecord::Schema.define(version: 2020_10_20_174830) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "prefecture_id"
-    t.string "city"
+    t.string "postal_code", null: false
+    t.string "prefectures", null: false
+    t.string "municipalities", null: false
+    t.string "building", null: false
+    t.string "phone_number"
+    t.string "address", null: false
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
@@ -36,16 +40,8 @@ ActiveRecord::Schema.define(version: 2020_10_28_114104) do
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "exhibition_id"
     t.string "image"
-    t.string "postal_code", null: false
-    t.string "prefectures", null: false
-    t.string "municipalities", null: false
-    t.string "building", null: false
-    t.string "phone_number"
-    t.string "address", null: false
-    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
