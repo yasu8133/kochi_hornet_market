8559d01fced975c226bcaeba31e73f84e0a62b00
yasu8133 <yasu.21198133@gmail.com>
2020-11-18 20:29:03 +0900

    a

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,14 @@
 class ItemsController < ApplicationController
+
+
   def index
   end
 
   def show
   end
+
+  private
+    def exhibition_params
+      params.require(:exhibition).permit(:shipping_charges, :shipping_area, :shipping_date, :price, :goods_status, :goods_name, :goods_demonstrate).merge(use_id: current_user.id)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_09_092358) do
+
+ActiveRecord::Schema.define(version: 2020_10_19_084955) do
+
+  create_table "exhibitions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "category_id", null: false
+    t.string "shipping_charges", null: false
+    t.string "shipping_area", null: false
+    t.string "shipping_date", null: false
+    t.string "price", null: false
+    t.string "goods_status", null: false
+    t.string "goods_name", null: false
+    t.string "goods_demonstrate", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
